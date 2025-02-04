b3b9e0704567c57431a99104d48c70842082e91e
finteger <nashassociatesinc@gmail.com>
2025-02-03 19:27:28 -0700

    Update

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -4,6 +4,8 @@ describe('Login Page', () =>{
       cy.visit('http://localhost:3000');
     });
 
+
+    //Positive testing
     it('should login with valid credentials', () =>{
       //Arrange
       cy.get('#email').type('test@example.com');
@@ -17,4 +19,20 @@ describe('Login Page', () =>{
         expect(txt).to.contains('Login Successful!')
       });
     });
+
+    //Negative testing
+    it('should display error message if wrong credentials', () =>{
+      //Arrange
+      cy.get('#email').type('test@examples.com');
+      cy.get('#password').type('password123');
+
+      //Act
+      cy.get('#login-button').click();
+
+      //Assert
+      cy.get('#error-message').should(
+        'contain',
+        'Invalid email or password'
+    );
+  });
 });
